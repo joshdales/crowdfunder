4eaf54aa4d907088566823dfde7c31a11b30acec
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-26 15:54:36 -0400

    Write test to check that project goal must be a positive number

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -33,6 +33,16 @@ class ProjectTest < ActiveSupport::TestCase
     assert project.invalid?, 'Project should have a goal that is greater than 0.'
   end
 
+  test 'project is invalid with goal < 0' do
+    owner = new_user
+    owner.save
+    project = new_project
+    project.user = owner
+    project.goal = -100
+    project.save
+    assert project.invalid?, 'Project should have a goal that is positive.'
+  end
+
   def new_project
     Project.new(
       title:       'Cool new boardgame',
