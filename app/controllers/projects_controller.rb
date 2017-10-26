6305914b965aedd3cfaecbc0b37fd0e0b72bfc25
Abby Christens <achristens@gmail.com>
2017-10-26 17:03:45 -0400

    Working on date:nil for new projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,8 +20,24 @@ class ProjectsController < ApplicationController
     @project.title = params[:project][:title]
     @project.description = params[:project][:description]
     @project.goal = params[:project][:goal]
-    @project.start_date = params[:project][:start_date]
-    @project.end_date = params[:project][:end_date]
+    # @project.start_date = params[:project][:start_date]
+    
+    @project.start_date = DateTime.new(
+      params[:project]["start_date(1i)"],
+      params[:project]["start_date(2i)"],
+      params[:project]["start_date(3i)"],
+      params[:project]["start_date(4i)"],
+      params[:project]["start_date(5i)"]
+    )
+
+    # @project.end_date = params[:project][:end_date]
+    @project.end_date = DateTime.new(
+      params[:project]["end_date(1i)"],
+      params[:project]["end_date(2i)"],
+      params[:project]["end_date(3i)"],
+      params[:project]["end_date(4i)"],
+      params[:project]["end_date(5i)"]
+    )
     @project.image = params[:project][:image]
     @project.user = current_user
 
