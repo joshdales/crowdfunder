b9d2e8af32621a652cc2bf3e22b15a8deba67b0d
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-27 12:16:46 -0400

    Add reward_id column to pledges table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171027043721) do
+ActiveRecord::Schema.define(version: 20171027161526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171027043721) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "project_id"
+    t.integer "reward_id"
     t.index ["project_id"], name: "index_pledges_on_project_id"
     t.index ["user_id"], name: "index_pledges_on_user_id"
   end
