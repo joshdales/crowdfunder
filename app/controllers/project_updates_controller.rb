f61b74af5cd4551ddf22f7320d33ab6bec59c438
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-27 13:08:16 -0400

    Set parmas to save project_update

--- app/controllers/project_updates_controller.rb
+++ app/controllers/project_updates_controller.rb
@@ -7,14 +7,27 @@ class ProjectUpdatesController < ApplicationController
   end
 
   def show
+    @update = ProjectUpdate.find(params[:project_update_id])
+    @project = Project.find(params[:project_id])
   end
 
   def edit
-    @update = ProjectUpdate.new
+    @update = ProjectUpdate.find(params[:project_update_id])
     @project = Project.find(params[:project_id])
   end
 
   def create
+    @update = ProjectUpdate.new
+    @project = Project.find(params[:project_id])
+    @update.project = @project
+    @update.title = params[:project_update][:title]
+    @update.description = params[:project_update][:description]
+
+    if @update.save
+      redirect_to project_url(@project)
+    else
+      render :new
+    end
   end
 
   def update
