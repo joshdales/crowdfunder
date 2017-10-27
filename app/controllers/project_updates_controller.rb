aeea0f87678ce68d4898bf120e8ff7949785cb81
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-27 12:33:50 -0400

    Create partial view for product_update form and set variables for new and edit in the contoller

--- app/controllers/project_updates_controller.rb
+++ app/controllers/project_updates_controller.rb
@@ -2,13 +2,16 @@ class ProjectUpdatesController < ApplicationController
   before_action :require_login, except: [:show]
 
   def new
-
+    @update = ProjectUpdate.new
+    @project = Project.find(params[:project_id])
   end
 
   def show
   end
 
   def edit
+    @update = ProjectUpdate.new
+    @project = Project.find(params[:project_id])
   end
 
   def create
