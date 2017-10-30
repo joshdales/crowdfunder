33b749b6a4ee7a0e6e214f8428acbc941e3e27b4
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-30 19:15:21 -0400

    Define ProjectComments create action to save a comment

--- app/controllers/project_comments_controller.rb
+++ app/controllers/project_comments_controller.rb
@@ -1,6 +1,17 @@
 class ProjectCommentsController < ApplicationController
   def create
-    #code
+    @project = Project.find(params[:project_id])
+    @project_comment = @project.project_comments.build
+    @project_comment.user = current_user
+    @project_comment.comment = params[:project_comment][:comment]
+
+    if @project_comment.save
+      flash[:notice] = "Thank you for submitting your comment!"
+      redirect_to project_url(@project)
+    else
+      flash[:alert] = "Sorry, there was a problem with submitting your comment!"
+      render "projects/show"
+    end
   end
 
   def destroy
