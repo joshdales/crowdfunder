a3b3279c447d271a87165c8c5e99c56920e97f43
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-30 17:36:22 -0400

    Update project_update tests with project factories

--- test/models/project_update_test.rb
+++ test/models/project_update_test.rb
@@ -5,11 +5,7 @@ class UpdateTest < ActiveSupport::TestCase
   # end
 
   test "Update can be created" do
-    owner = new_user
-    owner.save
-    project = new_project
-    project.user = owner
-    project.save
+    project = create(:project)
     update = new_update
     update.project = project
     update.save
@@ -17,54 +13,23 @@ class UpdateTest < ActiveSupport::TestCase
   end
 
   test "Update must be associated to a valid project" do
-    owner = new_user
-    owner.save
     update = new_update
     update.save
     assert update.invalid?, 'Update should have a project'
   end
 
   test "Update must have a valid title" do
-    owner = new_user
-    owner.save
-    project = new_project
-    project.user = owner
-    project.save
+    project = create(:project)
     project_update = ProjectUpdate.new(description: "It's all going well!")
     project_update.save
     assert project_update.invalid?, 'Update should have a title'
   end
 
   test "Update must have a valid description" do
-    owner = new_user
-    owner.save
-    project = new_project
-    project.user = owner
-    project.save
+    project = create(:project)
     project_update = ProjectUpdate.new(title: "Lowenthal")
     project_update.save
     assert project_update.invalid?, 'Update should have a title'
-  end
-
-
-  def new_project
-    Project.new(
-      title:       'Cool new boardgame',
-      description: 'Trade sheep',
-      start_date:  Date.today,
-      end_date:    Date.today + 1.month,
-      goal:        50000
-    )
-  end
-
-  def new_user
-    User.new(
-      first_name:            'Sally',
-      last_name:             'Lowenthal',
-      email:                 'sally@example.com',
-      password:              'passpass',
-      password_confirmation: 'passpass'
-    )
   end
 
   def new_update
