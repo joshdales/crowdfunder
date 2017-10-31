3f366d8c5c52b04e65265eef9602d404b76b43a5
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-31 10:17:25 -0400

    Update pledge tests with factories

--- test/factories/pledges.rb
+++ test/factories/pledges.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :pledge do
     dollar_amount 99.00
     project
-    user
     reward
+    user
   end
 end
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     first_name 'Sally'
     last_name 'Lowenthal'
-    sequence(:email) { |num| 'sally#{num}@example.com' }
+    sequence(:email) { |num|  "sally#{num}@example.com" }
     password 'passpass'
     password_confirmation 'passpass'
   end
