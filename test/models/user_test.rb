f9dd01dacc7354a2c919b3bb14eaf54748ef65cb
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-31 11:59:57 -0400

    Update user tests wth factories

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,23 +2,27 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "email must be unique" do
-    user = User.create(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "12345678")
-    user2 = User.new(email: "bettymaker@gmail.com", password: "00000000", password_confirmation: "00000000")
+    user = create(:user, email: "bettymaker@gmail.com")
+    user2 = build(:user, email: "bettymaker@gmail.com")
+    user2.save
     refute user2.valid?
   end
 
   test "user must include password_confirmation on create" do
-    user = User.new(email: "bettymaker@gmail.com", password: "12345678")
+    user = build(:user, password_confirmation: nil)
+    user.save
     refute user.valid?
   end
 
   test "password must match confirmation" do
-    user = User.new(email: "bettymaker@gmail.com", password: "12345678", password_confirmation: "87654321")
+    user = build(:user, password_confirmation: "87654321")
+    user.save
     refute user.valid?
   end
 
   test "password must be at least 8 characters long" do
-    user = User.new(email: "bettymaker@gmail.com", password: "1234", password_confirmation: "1234")
+    user = build(:user, password: "1234", password_confirmation: "1234")
+    user.save
     refute user.valid?
   end
 
