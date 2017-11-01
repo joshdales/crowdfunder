4ce898c9a8862ba0c93fce29e3966331e6e37aca
Demi Lee <demilee.to@gmail.com>
2017-11-01 18:32:49 -0400

    styled owner nav bar

--- app/assets/javascripts/owners.js
+++ app/assets/javascripts/owners.js
@@ -1,18 +1,29 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-document.getElementById('my-pledges').style.display = "none";
 document.getElementById('my-projects').style.display = "grid";
+document.getElementById('projects').style.backgroundColor = "white";
+document.getElementById('projects').style.color = "black";
+
+document.getElementById('my-pledges').style.display = "none";
+document.getElementById('pledges').style.backgroundColor = "grey";
 
 function toggleState(tab, section) {
   document.getElementById(tab).addEventListener('click', function() {
-    console.log('hello');
 
     document.getElementById('my-pledges').style.display = "none";
     document.getElementById('my-projects').style.display = "none";
 
-    document.getElementById(section).style.display = 'grid';
+    document.getElementById('pledges').style.backgroundColor = "grey";
+    document.getElementById('projects').style.backgroundColor = "grey";
 
+    document.getElementById('pledges').style.color = "white";
+    document.getElementById('projects').style.color = "white";
+
+
+    document.getElementById(section).style.display = 'grid';
+    document.getElementById(tab).style.backgroundColor = "white";
+    document.getElementById(tab).style.color = "black";
     // a = document.getElementsByClassName("project-list");
     // for (i=0; i<a.length; i++) {
     //   a[i].classList.toggle('changed');
